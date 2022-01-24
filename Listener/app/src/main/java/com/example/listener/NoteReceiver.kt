2c7c5c65fe2ae7725bd56da2ee417771f5a21af0
MinaevaEA <elka-vt@yandex.ru>
2022-01-24 21:19:21 +0300

    add listener

--- Listener/app/src/main/java/com/example/listener/NoteReceiver.kt
+++ Listener/app/src/main/java/com/example/listener/NoteReceiver.kt
@@ -1,4 +1,23 @@
 package com.example.listener
 
-class NoteResiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.TextView
+
+
+class NoteReceiver(private var notes: TextView) :
+    BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val newNote = intent?.getStringExtra(NOTE_TEXT)
+        val notesLog = notes.text.toString()
+        val result = "$newNote;\n$notesLog"
+        notes.text = result
+    }
+
+    companion object {
+        private const val NOTE_TEXT = "note_text"
+
+    }
 }
